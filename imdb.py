9fc1e781affb50089204ce56a53518efefa00445
punkrocker <punkrock_2000@163.com>
2019-08-07 21:56:03 +0800

    索引映射回原文

---
+++ imdb.py
@@ -0,0 +1,7 @@
+from keras.datasets import imdb
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+word_index = imdb.get_word_index()
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+decode_review = ' '.join([reverse_word_index.get(i - 3, '?') for i in train_data[0]])
+print(decode_review)
