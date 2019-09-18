833fc5dc123fb60b844d669d8bbca197b61d34ba
yuhaokui <punkrock_2000@163.com>
2019-09-18 17:48:16 +0800

    keras散列one-hot

--- rnn/keras_one_hot.py
+++ rnn/keras_one_hot.py
@@ -1,3 +1,4 @@
+import numpy as np
 from keras.preprocessing.text import Tokenizer
 
 samples = ['The cat sat on the mat.',
@@ -10,4 +11,12 @@ one_hot_results = tokenizer.texts_to_matrix(samples, mode='binary')
 word_index = tokenizer.word_index
 print('Found %s unique token.' % len(word_index))
 
+dimensionality = 1000
+max_length = 10
 
+results = np.zeros((len(samples), max_length, dimensionality))
+for i, sample in enumerate(samples):
+    for j, word in list(enumerate(sample.split()))[:max_length]:
+        index = abs(hash(word)) % dimensionality
+        results[i, j, index] = 1.
+print(results)
